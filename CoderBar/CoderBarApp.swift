39f78d8e59dc6f9dabe0f90bc7f3aa170839f8f0
Leon Martin <alquimista3301@gmail.com>
2026-09-04 11:41:35 -0600

    Initial Commit

---
+++ CoderBar/CoderBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoderBarApp.swift
+//  CoderBar
+//
+//  Created by Malva on 04/09/26.
+//
+
+import SwiftUI
+
+@main
+struct CoderBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
